45e691afc8eaebef2eadca139479f0c45488c18b
ras0q <ras0q@users.noreply.github.com>
2025-04-30 16:29:00 +0900

    add README generation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Today I Learned (TIL)
+
+## Index (1 entries, Newest first)
+
+- [python/2025-04-30_marimo](./python/2025-04-30_marimo)
---
+++ generate_index.ts
@@ -0,0 +1,20 @@
+import { globToRegExp } from "jsr:@std/path@1.0.9";
+import { walk } from "jsr:@std/fs@1.0.17";
+
+const glob = [
+  globToRegExp(
+    "*/[[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]]-[[:digit:]][[:digit:]]_*/",
+  ),
+];
+
+const tilEntries = (await Array.fromAsync(walk(".", { match: glob })))
+  .filter((e) => e.isDirectory)
+  .toSorted((a, b) => a.name === b.name ? 0 : a.name > b.name ? 1 : -1);
+
+const content = `# Today I Learned (TIL)
+
+## Index (${tilEntries.length} entries, Newest first)
+
+${tilEntries.map((e) => `- [${e.path}](./${e.path})`)}`;
+
+console.log(content);
