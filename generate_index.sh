2f5d707469e2e89977721b5a25eca36c25621fd3
ras0q <ras0q@users.noreply.github.com>
2025-07-19 18:06:21 +0900

    typo

--- generate_index.sh
+++ generate_index.sh
@@ -3,13 +3,13 @@
 find . -mindepth 2 -maxdepth 2 -type d | \
   grep -E './*/[0-9]{4}-[0-9]{2}-[0-9]{2}_*' | \
   awk -F'/' '{
-    if (match($3, /^{4}-[0-9]{2}-[0-9]{2}/, arr)) {
+    if (match($3, /^[0-9]{4}-[0-9]{2}-[0-9]{2}/, arr)) {
       print arr[0] "\t" $0
     }
   }' > /tmp/index_dirs_with_date.tmp
 
 sort -r /tmp/index_dirs_with_date.tmp | cut -f2- > /tmp/index_dirs.tmp
-count=$(wc -l < index_dirs.tmp)
+count=$(wc -l < /tmp/index_dirs.tmp)
 
 printf '# Today I Learned (TIL)\n\n'
 printf '## Index (%s entries, newest first)\n\n' "$count"
@@ -19,5 +19,5 @@ while IFS= read -r entry; do
   printf -- '- [%s](./%s)\n' "$entry_name" "$entry_name"
 done < /tmp/index_dirs.tmp
 
-rm -f /tmp/index_dirs_with_date.tmp /tmp/index_dirs.tmp
+# rm -f /tmp/index_dirs_with_date.tmp /tmp/index_dirs.tmp
 
