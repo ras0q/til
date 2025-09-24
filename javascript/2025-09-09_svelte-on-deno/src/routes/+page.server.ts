ee619357ae3548b05ac189923c70a5f82a3186f0
ras0q <ras0q@users.noreply.github.com>
2025-09-25 07:48:06 +0900

    fix(javascript/2025-09-09_svelte-on-deno): remove project

--- javascript/2025-09-09_svelte-on-deno/src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { env } from "$env/dynamic/private";
-import type { PageServerLoad } from "./$types.d.ts";
-
-export const load: PageServerLoad = () => {
-  return {
-    pwd: env.PWD,
-  };
-};
